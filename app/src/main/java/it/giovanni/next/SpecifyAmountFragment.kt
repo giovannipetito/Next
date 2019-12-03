ada8acc13c9b8e89f09e27cf41a666995db6b076
giovannipetito <gi.petito@gmail.com>
2019-12-04 00:09:33 +0100

    implement navigation components

---
+++ app/src/main/java/it/giovanni/next/SpecifyAmountFragment.kt
@@ -0,0 +1,57 @@
+package it.giovanni.next
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
+import java.math.BigDecimal
+
+class SpecifyAmountFragment : Fragment() , View.OnClickListener {
+
+    lateinit var navController: NavController
+    lateinit var destinatario: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        destinatario = arguments?.getString("recipient")!!
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_specify_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        send_btn.setOnClickListener(this)
+        cancel_btn.setOnClickListener(this)
+
+        val message = "Sending money to $destinatario"
+        recipient.text = message
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.send_btn -> {
+                if(!TextUtils.isEmpty(input_amount.text.toString())) {
+
+                    val amount = Money(BigDecimal(input_amount.text.toString()))
+                    val bundle = bundleOf("recipient" to destinatario, "amount" to amount)
+                    navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment, bundle)
+                }
+                else
+                    Toast.makeText(activity, "Enter an amount", Toast.LENGTH_SHORT).show()
+            }
+            R.id.cancel_btn -> activity!!.onBackPressed()
+        }
+    }
+}
